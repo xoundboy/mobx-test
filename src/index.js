257591f0a400f31e1a26dca16d06e14f4d6a3ea6
Ben Roberts <xoundboy@gmail.com>
2022-10-20 09:39:48 +0200

    output management in webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
-
+  const btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
   const myIcon = new Image();
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
